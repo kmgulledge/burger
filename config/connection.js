c9e75fcbf6129d6fd59d4d1ffd61acfd25d211f2
kmgulledge <kmgulledge@gmail.com>
2017-10-18 12:12:40 -0400

    please workkkk

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,24 @@
 
 // Node Dependency
 var mysql = require("mysql");
+
 /*
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "burgers_db"
-}); */
+});
+*/
+
+
 var connection = mysql.createConnection({
     host: "ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "fbsyq1ongmea1pcl",
     password: "oryv4x6nqcdgelyh",
     database: "fk8cj4s3vh6gau53"
 });
+
 
 connection.connect(function(err) {
     if (err) {
